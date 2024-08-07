a2965b2056db5a807e410ef08da278ba4e0a1126
Gilcecler Carneiro <gilcecler@gmail.com>
2024-08-07 14:31:38 -0300

    classe entitie e primeiro teste do construtor

---
+++ src/tests/FinancingTests.java
@@ -0,0 +1,28 @@
+package tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import entities.Financing;
+
+public class FinancingTests {
+
+	@Test
+	public void constructorShouldCreateObjectWhenValidData() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(100000.0, f.getTotalAmount());
+		Assertions.assertEquals(2000.0, f.getIncome());
+		Assertions.assertEquals(80, f.getMonths());
+	}
+	
+	@Test
+	public void constructorShouldThrowIllegalArgumentExceptionWhenInvalidData() {
+		
+		Assertions.assertThrows(IllegalArgumentException.class, () -> {
+			Financing f = new Financing(100000.0, 2000.0, 20);
+		});
+	}
+	
+}
