6a9f6f6aeacab2c61cbf482838cf12baa235d5b9
Gilcecler Carneiro <gilcecler@gmail.com>
2024-08-07 15:17:00 -0300

    quota-entry-test

--- src/tests/FinancingTests.java
+++ src/tests/FinancingTests.java
@@ -91,6 +91,21 @@ public class FinancingTests {
 		});
 	}
 
+	@Test
+	public void entryShouldCalculateEntryCorrectly() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(20000.0, f.entry());
+	}
+	
+	@Test
+	public void quotaShouldCalculateQuotaCorrectly() {
+		
+		Financing f = new Financing(100000.0, 2000.0, 80);
+		
+		Assertions.assertEquals(1000.0, f.quota());
+	}
 
 	
 }
